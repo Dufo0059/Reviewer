b61cfebfcc6449afc6e86d825557130f64055417
Dufo0059 <dufo0059@algonquinlive.com>
2017-04-19 20:07:33 -0400

    First upload
    Reviewer application

---
+++ platforms/ios/MyApp.xcarchive/Products/Applications/MyApp.app/www/js/test.js
@@ -0,0 +1,481 @@
+/*****************************************************************
+File: index.js
+Author: Jake Oh
+Description:
+Here is the sequence of logic for the app
+- Once Device ready, will read localStorage
+Version: 0.0.1
+Updated: Apr 3, 2017
+- 
+*****************************************************************/
+const MYKEY = "reviewr-oh000024";
+
+let greviews = [];
+
+var grating = 1;
+var stars = null;
+
+let regNumber = /^[0-5]$/;
+let greviewId = 0;
+let imagepath = "";
+var messages = [
+	{
+		"type": "bad",
+		"msg": "We are out of beer"
+	},
+	{
+		"type": "good",
+		"msg": "I just ordered pizza"
+	},
+	{
+		"type": "info",
+		"msg": "Today is Friday"
+	}
+];
+var REVIEW = {
+	id: '',
+	name: '',
+	rating: 0,
+	img: ''
+};
+
+
+function addListeners(){
+  [].forEach.call(stars, function(star, index){
+    star.addEventListener('click', (function(idx){
+      console.log('adding listener', index);
+      return function(){
+        grating = idx + 1;  
+        //console.log('Rating is now', grating)
+        setRating();
+      }
+    })(index));
+  });
+}
+
+function setRating(){
+  [].forEach.call(stars, function(star, index){
+    if(grating > index){
+      star.classList.add('rated');
+      //console.log('added rated on', index );
+    }else{
+      star.classList.remove('rated');
+      //console.log('removed rated on', index );
+    }
+  });
+}
+var app = {
+	localNote: null,
+	init: function () {
+		try {
+			console.logO("Start app's initialization")
+			document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+		} catch (e) {
+			document.addEventListener('DOMContentLoaded', this.onDeviceReady.bind(this), false);
+			console.log('failed to find deviceready');
+		}
+	},
+	onDeviceReady: function () {
+		console.log("called onDeviceReady");
+		console.log(navigator.camera);
+		//MODALHANDLER.init();
+		GSTORAGE.init();
+
+		let camBtn = window.document.querySelector('.btn.btn-primary.btn-block');
+		camBtn.addEventListener('touchstart', this.onCamera);
+
+		let CanBtn = window.document.querySelector('#cancel.btn.btn-block');
+		CanBtn.addEventListener('touchstart',(function(mname){return function(){app.onCancel(mname);}})("ReviewModal"));
+//		CanBtn.addEventListener('touchstart', this.onCancel);
+
+		let saveBtn = window.document.querySelector('#save.btn.btn-positive');
+		saveBtn.addEventListener('touchstart', this.onSave);
+				
+		window.document.querySelector("a.icon.icon-left-nav.pull-left").addEventListener('touchstart',(function(mname){return function(){app.onCancel(mname);}})("DeleteReviewModal"));
+		
+		window.document.querySelector(".btn.btn-link.btn-nav.pull-right").addEventListener("touchstart",function(){
+			HTMLHANDLER.clearModal();
+		});
+		
+		window.document.querySelector("#ReviewModal a.icon.icon-close.pull-right").addEventListener("touchstart",function(){
+			app.onCancel("");
+		});
+			
+		//window.addEventListener('push', app.pageChanged);
+	   stars = document.querySelectorAll('.star');
+	   addListeners();
+  		  //setRating(); //based on global rating variable value
+
+		HTMLHANDLER.createReviewsList();
+	},
+	onDelete: function (par, target) {
+	
+		let _pro = new Promise(
+			function(resolve,reject){
+				
+				console.log("Before count: " + GSTORAGE.gdata.length);
+
+				//app.generateMessage("#delreview","info","Review Deleted");
+				GSTORAGE.gdata = GSTORAGE.gdata.filter(function (p) {
+					return p.id != greviewId;
+				});
+				localStorage.setItem(MYKEY, JSON.stringify(GSTORAGE.gdata));
+
+				let review = document.querySelector("#DeleteReviewModal div.content-padded");
+				review.firstElementChild.classList.add("fadeout");
+
+				greviewId = 0;
+				par.removeChild(par.childNodes[0]);
+
+				console.log("After count: " + GSTORAGE.gdata.length);	
+				setTimeout(function(){
+					resolve();
+				},500);
+			});
+		
+		_pro.then(function(){
+			app.onCancel("DeleteReviewModal");
+			HTMLHANDLER.createReviewsList();
+		})
+		.catch(function(mesage){
+			   alert(message);
+			   })
+		
+	},
+	onCancel: function (m) {
+		
+		try{
+		console.log("called onCancel");
+		
+		if("DeleteReviewModal" === m){
+			let p = document.querySelector("#DeleteReviewModal p.content-padded");
+			for(let i=0,j=p.childElementCount;i<j;i++){
+				p.removeChild(p.firstElementChild);
+			}
+		}else{
+			let thumb = document.querySelector(".mid-thumb");
+			let img = document.querySelector("img#thumbnail");
+			img.src="";
+			thumb.style.display="none";
+			img.style.display = "none";
+
+		}
+		let modal = document.getElementById(m);
+		modal.classList.remove('active');
+		} catch(e){
+			app.generateMessage(e.message);
+		}finally{
+			greviewId=0;
+			grating=1;
+			HTMLHANDLER.clearModal();
+		}
+
+	},
+	onSave: function () {
+		
+		try{
+			console.log("called onSave");
+			let name = document.getElementById("name").value;
+			let rating = grating;//document.getElementById("rate").value;
+			//let retname = name &&0xff;
+			//let retrating = rating && 0xff;
+			if (!(name && 0xff)) {
+//				app.generateMessage("#ReviewModal div.content","bad","Please enter a name of Item");
+				app.generateMessage("#ReviewModal form.input-group","bad","Please enter a name of Item");
+				return;
+			}
+			
+			if(!(imagepath && 0xff)){
+			   	app.generateMessage("#ReviewModal div.content","bad","Please take a picture for Item")
+			   	return;
+			   }
+
+			let review = Object.create(REVIEW);
+			if (review === null) {
+
+				throw new Error("Create Object Error.Try it agai");
+			}
+			review.name = name;
+			review.rating = rating;
+			review.id = Date.now();
+			review.img = imagepath;
+
+			GSTORAGE.saveReview(review);
+
+			let thumb = document.querySelector(".mid-thumb");
+			let img = document.querySelector("img#thumbnail");
+
+			thumb.style.display="none";
+			img.style.display = "none";
+
+			let modal = document.getElementById('ReviewModal');
+			modal.classList.remove('active');
+			//		HTMLHANDLER.addEventListener("Update",function(){
+			//			console.log("Called CustomEvent Update");
+			//			return HTMLHANDLER.createReviewsList();
+			//		});
+			//		let evetn = new CustomEvent("Update");
+			//		HTMLHANDLER.dispatchEvent(event);
+
+		}catch(e){
+			app.generateMessage("Create Object Error.Try it again");
+		}finally{
+			HTMLHANDLER.createReviewsList();
+			grating = 1;
+		}
+	},
+	onCamera: function () {
+		console.log("Operating a camera");
+
+		var options = {
+			quality: 80,
+			destinationType: Camera.DestinationType.DATA_URL,
+			encodingType: Camera.EncodingType.PNG,
+			mediaType: Camera.MediaType.PICTURE,
+			pictureSourceType: Camera.PictureSourceType.CAMERA,
+			allowEdit: true,
+			targetWidth: 300,
+			targetHeight: 300
+		}
+		navigator.camera.getPicture(app.onSuccess, app.onError, options);
+	},
+	onSuccess: function (imageData) {
+		console.log("successfull.");
+		//var image = document.querySelector("media-object");
+		//app.generateMessage(imageData);
+		//.src = imageData;
+		imagepath = "data:image/png;base64," + imageData; //"data:image/jpeg;base64,"
+		
+		
+		let img = document.createElement("img");
+		img.classList.add("mid-thumb");
+		
+		let modalpad= document.querySelector("#ReviewModal p.content-padded");
+		
+		let thumb = document.querySelector(".mid-thumb");
+		//let img = document.querySelector("#thumbnail img");
+		
+		
+		let picbut = document.querySelector("#ReviewModal.btn.btn-primary");
+		picbut.style.visibility="hidden";
+		
+		
+		
+		img.src = imagepath;
+		img.style.width ="100%";
+		img.style.height="100%";
+
+		thumb.style.display="block";
+		img.style.display = "block";
+		//console.log(imageData);
+		
+		HTMLHANDLER.replaceBut(img);
+
+	},
+	onError: function (obj,type="bad",message) {
+//		console.log(message);
+		app.generateMessage(obj,type="bad",message);
+	},
+	generateMessage: function (obj,type="bad",message) {
+		let mcontent = window.document.querySelector(obj);
+		//let mcontentpad = window.document.querySelector('#ReviewModal div.content-padded');
+		let div = document.createElement('div');
+
+		div.classList.add('msg');
+		setTimeout(function () {
+			div.classList.add(type);
+		}, 20); //delay before adding the class to trigger transition
+
+		div.textContent = message===null?"Unknown Error":message;
+
+		mcontent.insertBefore(div, mcontent.firstElementChild[0]);
+		setTimeout((function (m, d) {
+			return function () {
+				m.removeChild(d);
+			}
+		})(mcontent, div), 3210);
+	}
+};
+const GSTORAGE = {
+	storage: "",
+	gdata: null,
+	init: function () {
+		try {
+			console.log("Start GSTROGAGE Initialization");
+			this.gdata = [];
+			this.storage = localStorage.getItem(MYKEY);
+
+			if (this.storage === null) {
+				console.log("LocalStorage is Empty");
+				//this.gdata=greviews;
+				//localStorage.setItem(MYKEY,JSON.stringify(this.gdata));
+			} else {
+				this.gdata = JSON.parse(this.storage);
+				console.log("FIRST DATA: " + this.gdata);
+
+				//this.sortData(this.gdata);
+				console.log("LATER DATA: " + this.gdata);
+			}
+			console.log("Finish GSTROGAGE Initialization");
+		} catch (e) {
+			console.log(e.message);
+		}
+	},
+	saveReview: function (review) {
+		try {
+			console.log("Save Picture");
+			this.gdata.push(review);
+			localStorage.setItem(MYKEY, JSON.stringify(this.gdata));
+			console.log("Successfully Update : " + review.name + "," + review.rating);
+			//			}
+		} catch (e) {
+			console.log(e);
+			app.generateMessage(e.message);
+		}
+	},
+
+	// When Gifts was updated..
+	deleteReview: function (id) {
+		try {
+			for (let i = 0, j = this.gdata.length; i < j; i++) {
+				if (id == this.gdata[i].id) {
+					console.log("BEFORE: " + this.gdata[i].ideas);
+					this.gdata[i].ideas = this.gdata[i].ideas.filter(function (p) {
+						return p.idea != idea;
+					});
+					localStorage.setItem(MYKEY, JSON.stringify(this.gdata));
+					console.log("AFTER: " + this.gdata[i].ideas);
+					break;
+				}
+				console.log("Contine");
+			}
+		} catch (e) {
+			console.log(e.message);
+		}
+	}
+
+}
+const HTMLHANDLER = {
+	createReviewsList: function () {
+		try {
+			let ul = document.querySelector(".table-view");
+			ul.innerHTML = "";
+			for (let item of GSTORAGE.gdata) {
+
+				//console.log("review: " + item.img);
+
+				let li = document.createElement("li");
+				let aNav = document.createElement("a");
+				let aImg = document.createElement("img");
+				let aDiv = document.createElement("div");
+				let aP = document.createElement("p");
+
+				li.classList.add("table-view-cell", "media");
+
+				aNav.classList.add("navigate-right");
+				aNav.href = "#DeleteReviewModal";
+
+				// To set up Uniq ID
+				let att = document.createAttribute("data-id");
+				att.value = item.id;
+				aNav.setAttributeNode(att);
+
+				aNav.addEventListener("touchstart", (function (u, l) {
+
+					return function () {
+						//var a = ev.currentTarget;
+						greviewId = aNav.getAttribute("data-id");
+						console.log("Clicked ID is " + greviewId);
+						let pa = u;
+						let tar = l;
+						for (let i = 0, j = GSTORAGE.gdata.length; i < j; i++) {
+
+							if (greviewId == GSTORAGE.gdata[i].id) {
+								let review = GSTORAGE.gdata[i];
+								let pad = document.querySelector("#DeleteReviewModal p.content-padded");
+								let div = document.querySelector("#delreview");
+
+								let pName = document.createElement("p");
+								let aName = document.createElement("a");
+
+								let img = document.createElement("img");
+								img.src = review.img;
+								img.style.width="100%";
+								img.style.width="100%";
+
+								pad.classList.remove("fadeout");
+								pName.textContent = review.name;
+								pName.style.fontSize="2.0rem";
+								pName.style.marginTop="1rem";
+								pName.style.marginBottom="1em";
+								
+								aName.appendChild(img);
+								pad.appendChild(aName);
+								pad.appendChild(pName);
+
+								let delBtn = document.querySelector('#delete.btn');
+
+								delBtn.addEventListener('touchstart', (function (ul, li) {
+									
+									return function(){app.onDelete(ul,li)};
+								})(pa, tar));
+
+								break;
+							}
+						}
+					}
+				})(ul, li));
+
+				aImg.classList.add("media-object", "pull-left");
+				aImg.src = item.img;
+				aImg.style.width="80px";
+				aImg.style.height="80px";
+				aDiv.classList.add("media-body");
+				aDiv.textContent = item.name;
+				//aP.textContent = item.rating;
+				
+				for(let i =0,j=item.rating;i<j;i++){
+					let span = document.createElement("span");
+					//span.textContent = "&nbsp;";
+					span.classList.add('star');
+					span.classList.add('rated');
+					span.classList.add('reviewp');
+					//span.textContent='&#9733';
+					span.style.fontSize="1.6rem";
+					aP.appendChild(span);
+				}
+				
+				aDiv.appendChild(aP);
+				aNav.appendChild(aImg);
+				aNav.appendChild(aDiv);
+				li.appendChild(aNav);
+				ul.appendChild(li);
+			}
+
+		} catch (e) {
+			HTMLHANDLER.generateMessage(e.message);
+		}
+
+	},
+	replaceBut:function(img){
+		
+		let but = document.getquerySelector("#ReviewModal button.btn.btn-primary.btn-block");
+		but.style.display = "none";
+		
+		let modalpad = document.getquerySelector("#ReviewModal p.content-padded");
+		modalpad.insertBefore(img,modalpad.childElement(1));
+		
+	},
+	clearModal:function(){
+		document.getElementById("name").value="";
+		grating	= 1;
+		setRating();
+		imagepath="";
+		navigator.camera.cleanup(function(){}, app.onError);
+	
+		let but = document.querySelector("#ReviewModal button.btn.btn-primary.btn-block");
+		but.style.display = "block";
+	}
+}
+
+app.init();
---
+++ www/js/index.js
@@ -0,0 +1,342 @@
+"use strict";
+
+
+
+const BENSKEY = "reviewer-ben";
+var reviewList = [];
+
+var REVIEW = {
+    id: '',
+    name: '',
+    starRating: 0,
+    image: ''
+};
+
+var idR = 0;
+var grating = 3;
+var stars = null;
+var globalId = 0;
+var camPic = "";
+
+
+document.addEventListener('deviceready', onDeviceReady);
+
+
+
+function onDeviceReady() {
+
+    window.document.querySelector('#save').addEventListener('touchstart', saveButton);
+    window.document.querySelector('#cancel').addEventListener('touchstart', cancelButton);
+    window.document.querySelector('#picture').addEventListener('touchstart', pictureButton);
+    document.querySelector('#redButton').addEventListener('touchstart', deleteButton);
+    if (localStorage.getItem(BENSKEY) == null) {
+        localStorage.setItem(BENSKEY, JSON.stringify(reviewList));
+    }
+    var value = localStorage.getItem(BENSKEY);
+    reviewList = JSON.parse(value);
+    console.trace(reviewList);
+
+
+    showReviews();
+    addListeners();
+    setRating();
+
+}
+
+function saveButton() {
+    console.log('called saveButton')
+        //
+    var name = document.getElementById("name");
+    var starRating = grating; //.toString();
+    //
+    if (name.value.length == 0) {
+        alert("not completed review");
+        //        return;
+    }
+    //
+    //	//If getting base information, process a next step
+    let touchEnd = new CustomEvent('touchend', {
+        bubbles: true,
+        cancelable: true
+    });
+    document.querySelector("#addReview .icon.icon-close.pull-right").dispatchEvent(touchEnd);
+
+    let review = null;
+    review = Object.create(REVIEW);
+    review.id = Date.now();
+    review.name = name.value;
+    review.starRating = grating; //.toString();	
+    review.image = camPic;
+    reviewList.push(review);
+
+
+
+    localStorage.setItem(BENSKEY, JSON.stringify(reviewList));
+    
+    
+    name.value = "";
+    var image = document.getElementById('myImage');
+    image.src = "";
+ 
+
+    showReviews();
+
+
+}
+
+
+function pictureButton() {
+    console.log('called pictureButton');
+	var options = {
+		quality: 80,
+		destinationType: Camera.DestinationType.FILE_URI,
+		encodingType: Camera.EncodingType.PNG,
+		mediaType: Camera.MediaType.PICTURE,
+		pictureSourceType: Camera.PictureSourceType.CAMERA,
+		allowEdit: true,
+		targetWidth: 300,
+		targetHeight: 300
+	}
+
+    navigator.camera.getPicture(onSuccess, onFail, options);
+}
+
+function onSuccess(imageURI) {
+
+
+    console.log("successfull.");
+
+
+    //    camPic = "data:image/png;base64," + imageURI;
+
+    var image = document.getElementById('myImage');
+    //image.src ="data:image/png;base64," + imageURI;
+    image.src =imageURI;
+	image.style.width="100%";
+	image.style.height="100%";
+    camPic = imageURI;
+    
+
+
+
+
+
+    //    console.log(imageURI);
+}
+
+function onFail(message) {
+    alert('Failed because: ' + message);
+}
+
+
+
+
+
+function cancelButton() {
+    console.log('called cancelButton');
+
+    let touchEnd = new CustomEvent('touchend', {
+        bubbles: true,
+        cancelable: true
+    });
+    document.querySelector("#addReview .icon.icon-close.pull-right").dispatchEvent(touchEnd);
+}
+
+
+function deleteButton(ev) {
+
+
+
+    console.log("delete called")
+
+
+var id = ev.currentTarget.getAttribute("itemId");
+    
+    var index = null;
+    for (var i = 0; i < reviewList.length; i++) {
+        
+        if (id == reviewList[i].id){
+            
+            index=i;
+            
+        }
+        
+    }
+    
+    if (index != null){
+        
+        reviewList.splice(index,1);
+        
+        localStorage.setItem(BENSKEY, JSON.stringify(reviewList));
+        
+    }
+    
+    
+showReviews();
+
+    //clear img on page
+    //find item in reviewList and remove from the array with splice()
+    //update localStorage  with setItem()
+    //call the function to showReviews ... which uses reviewList to display the list
+
+    //    reviewList.splice(2);
+    //    localStorage.setItem(BENSKEY, JSON.stringify(reviewList));
+    //
+    //    showReviews();
+
+
+    var touchEnd = new CustomEvent('touchend', {
+        bubbles: true,
+        cancelable: true
+    });
+    document.querySelector("#removeR .icon.icon-close.pull-right").dispatchEvent(touchEnd);
+
+}
+
+
+function showReviews() {
+
+    stars = document.querySelectorAll('.star');
+    document.getElementById("review-list").innerHTML = "";
+    //	reviewList.forEach(function (review) {
+    for (let review of reviewList) {
+
+        var li = document.createElement('li');
+        li.classList.add('table-view-cell');
+
+        var span1 = document.createElement("span");
+        span1.classList.add("name");
+        var aInName = document.createElement("a");
+        aInName.href = "#removeR";
+        aInName.textContent = review.name;
+        aInName.setAttribute("clickforD", review.id)
+        var img = document.createElement("img");
+        img.className = "reviewImg";
+        //img.src = "data:image/png;base64," + review.image;
+        img.src = review.image;
+
+        //        li.appendChild(img);
+        // var c = document.getElementById("c");
+        // var ctx = c.getContext("2d")
+        //var i = document.getElementById("i");
+        //ctx.drawImage(i,0,0,50,50);
+        //ctx.drawImage(i,50,50,10,10)
+
+
+
+
+        //
+        //        let att = document.createAttribute("data-id");
+        //        att.value = review.id;
+        //
+        //        aInName.setAttributeNode(att);
+        aInName.addEventListener('touchstart', function (ev) {
+
+
+            document.getElementById("appendN").innerHTML = "";
+
+            var pInName = document.createElement("li");
+            pInName.textContent = review.name;
+            document.getElementById("appendN").appendChild(pInName);
+            var img2 = document.createElement("img2");
+            img2.className = "reviewImg2";
+            //img2.src = "data:image/png;base64," + review.image;
+            img2.src = review.image;
+            
+            var redButton = document.createElement("button");
+            redButton.classList.add("btn", "btn-block");
+            redButton.id = "redButton";
+            var spanB = document.createElement("span");
+            spanB.classList.add("icon", "icon-close");
+            redButton.textContent = ("remove");
+            redButton.appendChild(spanB);
+            pInName.appendChild(img);
+
+
+            var li2 = document.createElement('li');
+            for (let i = 0, j = review.starRating; i < j; i++) {
+
+                var span5 = document.createElement("span");
+                span5.classList.add("star", "rated");
+                li2.appendChild(span5);
+
+                //span2.textContent = "&nbsp;";
+            }
+
+           redButton.setAttribute ("itemId", review.id ); 
+            redButton.addEventListener('touchstart', function (ev) {
+                
+                
+                deleteButton(ev);
+
+            });
+
+            document.getElementById("appendN").appendChild(li2);
+            document.getElementById("appendN").appendChild(redButton);
+
+
+
+
+            //            localStorage.removeItem(BENSKEY);
+
+
+        });
+
+
+        li.appendChild(span1);
+        for (let i = 0, j = review.starRating; i < j; i++) {
+
+            var span2 = document.createElement("span");
+            span2.classList.add("star", "rated");
+            li.appendChild(span2);
+
+            //span2.textContent = "&nbsp;";
+        }
+        span1.appendChild(img);
+        span1.appendChild(aInName);
+
+
+        document.getElementById("review-list").appendChild(li);
+
+
+
+    }
+    // var grating = 0;
+
+
+}
+
+//global declarations
+
+//initial setup
+//document.addEventListener('DOMContentLoaded', function () {
+//    stars = document.querySelectorAll('.star');
+//    addListeners();
+//    setRating(); //based on global rating variable value
+//});
+
+function addListeners() {
+  [].forEach.call(stars, function (star, index) {
+        star.addEventListener('click', (function (idx) {
+            console.log('adding listener', index);
+            return function () {
+                grating = idx + 1;
+                console.log('Rating is now', grating)
+                setRating();
+            }
+        })(index));
+    });
+
+}
+
+function setRating() {
+  [].forEach.call(stars, function (star, index) {
+        if (grating > index) {
+            star.classList.add('rated');
+            console.log('added rated on', index);
+        } else {
+            star.classList.remove('rated');
+            console.log('removed rated on', index);
+        }
+    });
+}
